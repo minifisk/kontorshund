267500be137385b7c79f809cf5018f4b96ef8a18
Alexander Lindgren <alexander.f.lindgren@gmail.com>
2021-11-30 19:49:07 +0100

    Fixed back-end functionality for submitting new ad form

--- core/forms.py
+++ core/forms.py
@@ -10,3 +10,19 @@ class AdvertisementForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['municipality'].queryset = Municipality.objects.none()
         self.fields['area'].queryset = Area.objects.none()
+        self.fields['area'].required = False
+
+        if 'province' in self.data:
+            try:
+                # Set municipality queryset
+                province_id = int(self.data.get('province'))
+                self.fields['municipality'].queryset = Municipality.objects.filter(province_id=province_id).order_by('name')
+            
+                # Set area queryset
+                municipality_id = int(self.data.get('municipality'))
+                self.fields['area'].queryset = Area.objects.filter(municipality_id=municipality_id).order_by('name')
+
+            except (ValueError, TypeError):
+                pass # invalid input from the client; ignore and fallback to empty Municipality/Area queryset
+            
+
