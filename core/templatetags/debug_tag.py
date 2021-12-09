c5c4d27744bfcea685ad58c2965b17bc9600dc99
Alexander Lindgren <alexlindgren08@gmail.com>
2021-12-09 21:00:01 +0100

    Minor changes

--- core/templatetags/debug_tag.py
+++ core/templatetags/debug_tag.py
@@ -4,3 +4,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def set_breakpoint(context):
     breakpoint()
+
+
+## Usage 
+## {% load debug_tag %}
+##  {% set_breakpoint %}
