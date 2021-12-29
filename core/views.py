b9ccdafa0f3b68886747001200fc7fb51549183f
Alexander Lindgren <alexlindgren08@gmail.com>
2021-12-29 16:09:34 +0100

    commeting out ngrok

--- core/views.py
+++ core/views.py
@@ -235,15 +235,11 @@ def GenerateSwishPaymentRequestToken(request, pk):
 
 def GenerateSwishPaymentQrCode(request, pk):
 
-    # Generate callback url if development
-    if bool(os.environ.get('IS_DEVELOPMENT')) == True:
-        SWISH_CALLBACKURL = urljoin(NGROK_URL, "/swish/callback")
+    #SWISH_CALLBACKURL = urljoin(NGROK_URL, "/swish/callback")
 
-    # Generate callback url if production
-    if bool(os.environ.get('IS_DEVELOPMENT')) == False:
-        url = request.build_absolute_uri('/')
-        callback_path = f'swish/callback'
-        SWISH_CALLBACKURL= f'{url}{callback_path}'
+    url = request.build_absolute_uri('/')
+    callback_path = f'swish/callback'
+    SWISH_CALLBACKURL= f'{url}{callback_path}'
 
     # Set-up variables for payment request
     payload = {
