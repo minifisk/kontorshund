c43544069665f6f08fee55c6e856cdf442e2fcec
Alexander Lindgren <alexlindgren08@gmail.com>
2022-01-25 21:26:04 +0100

    Trying to get supercronic to work with crontabs related to django...

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello crontab")
+
+
+from django.db import connection
+from django.conf import settings
+
+settings.configure()
+
+from core.models import Advertisement
+
+count = Advertisement.objects.all().count()
+
+print('Number of advertisements: ', count)
