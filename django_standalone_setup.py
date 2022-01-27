93ba6991263351741fd39ae5af28c7c4ea6e02a5
Alexander Lindgren <alexlindgren08@gmail.com>
2022-01-27 20:54:10 +0100

    removed redundant file

--- django_standalone_setup.py
+++
@@ -1,5 +0,0 @@
-""" Setting up Django to work as stand-alone for supercronic in container """
-
-import sys, os, django, time
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kontorshund.settings")
-django.setup()
