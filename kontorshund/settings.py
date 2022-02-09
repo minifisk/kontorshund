c69284ce45f7aadd484063c7009027ad34d0b59d
Alexander Lindgren <alexlindgren08@gmail.com>
2022-02-09 19:33:43 +0100

    Commented out SSL settings temporary while working on a new certificate structire

--- kontorshund/settings.py
+++ kontorshund/settings.py
@@ -417,12 +417,12 @@ if (os.environ.get('IS_DEVELOPMENT')) == 'FALSE':
     SERVER_EMAIL = 'server@kontorshund.se'
 
     print('Using secure SSL settings')
-    SECURE_SSL_REDIRECT = True
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 3600
-    SECURE_HSTS_PRELOAD = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_SSL_REDIRECT = True
+    # CSRF_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
+    # SECURE_HSTS_SECONDS = 3600
+    # SECURE_HSTS_PRELOAD = True
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
     # Sentry
     sentry_sdk.init(
