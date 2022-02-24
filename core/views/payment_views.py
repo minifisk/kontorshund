a5a3f8c67ddce9d4a663ebff5d41a34cf9106644
Alexander Lindgren <alexlindgren08@gmail.com>
2022-02-24 21:21:23 +0100

    Removed redundant prints

--- core/views/payment_views.py
+++ core/views/payment_views.py
@@ -173,11 +173,7 @@ class SwishCallback(View):
 
                 # If deletion date is later than today, add 30 days ontop of that
                 if get_one_month_ahead_from_date_obj(ad_obj.deletion_date) > new_deletion_date:
-                   # print('old deletion date', ad_obj.deletion_date)
-                   # print('new deletion date', new_deletion_date)
-                   # print('test!')
                     new_deletion_date = get_one_month_ahead_from_date_obj(ad_obj.deletion_date)
-                   # print('extra new deletion date', new_deletion_date)
 
                 logger.debug(
                     f'Setting deletion date for ad {ad_obj.pk} to {new_deletion_date} for when Swish callback view was requested, ad id {ad_obj.pk} amount {amount} payment_reference {payment_reference} payer alias {payer_alias}'
