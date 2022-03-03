559fab69a5a3c03de12ae5ceaf31c82b91275116
Alexander Lindgren <alexlindgren08@gmail.com>
2022-03-03 21:14:17 +0100

    Updated test news_email management command with correct setup

--- core/tests/test_management_commands.py
+++ core/tests/test_management_commands.py
@@ -4,6 +4,7 @@ import os
 from venv import create
 import datetime
 from dateutil.relativedelta import *
+from random import randrange
 
 from django.core.management import call_command
 from django.conf import settings
@@ -17,6 +18,8 @@ from core.abstracts import prevent_request_warnings
 from core.views.payment_views import SwishCallback, logger, get_qr_code
 from core.tests.factories import create_swish_callback_payload
 from core.models import Area
+from core.tests.factories import create_news_email
+from core.models import Province, Municipality, IntervalChoices, AdTypesChoices
 
 from kontorshund.settings import PRICE_SWISH_INITIAL, PRICE_SWISH_EXTEND
 
@@ -52,140 +55,115 @@ class TestSetupCommands(TestCase):
         cls.user1.save()
         cls.user2.save()
 
-        cls.user_dict = {}
 
-        from random import randrange
-        username = f'asdf {randrange(100)}'
-        password = '1234'
+
+
+        def create_users(number_of_users=1):
             
-        User.objects.create_user(username=username, password=password)
+            cls.user_dict = {}
 
-        # def create_users(number_of_users=1):
-        #     from random import randrange
-        #     username = f'asdf {randrange(100)}'
-        #     password = '1234'
-            
-        #     for x in range(number_of_users):
-        #         cls.user_dict[x] = User.objects.create_user(username=username, password=password)
+            for i in range(number_of_users):
+                username = f'asdf {randrange(10000)}'
+                password = '1234'
+                cls.user_dict[f'user_{i}'] = User.objects.create_user(username=username, password=password)
 
-        # create_users(number_of_users=1)
-
-        # print(cls.user_dict)
-
-            
-
-        # from core.tests.factories import create_news_email
-        # from core.models import Province, Municipality, IntervalChoices, AdTypesChoices
+        create_users(number_of_users=8)
         
-        # # SUBSCRIPTIONS
-        # cls.province = Province.objects.get(name='Stockholm'),
-        # cls.municipality = Municipality.objects.get(name='Stockholms stad'),
-        # cls.area = Area.objects.get(name='Enskede, Årsta, Skarpnäck')
+        # SUBSCRIPTIONS
+        cls.province = Province.objects.get(name='Stockholm')
+        cls.municipality = Municipality.objects.get(name='Stockholms stad')
+        cls.area = Area.objects.get(name='Enskede, Årsta, Skarpnäck')
 
-        # ##### Offering ######
-        # cls.ad_type_offering = AdTypesChoices.OFFERING
+        ##### Offering ######
+        cls.ad_type_offering = AdTypesChoices.OFFERING
 
-        # ### Daily 
-        # cls.interval_daily = IntervalChoices.DAILY
+        ### Daily 
+        cls.interval_daily = IntervalChoices.DAILY
         
-        # #### subscription without areas
-        # cls.news_email_daily_offering_without_area = create_news_email( 
-        #     user=cls.user_dict[0],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_daily,
-        #     ad_type=cls.ad_type_offering,
-        #     is_active=True
-        # )
-        
-        # #### subscription with areas
-        # cls.news_email_daily_offering_with_area = create_news_email( 
-        #     user=cls.user_dict[1],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_daily,
-        #     ad_type=cls.ad_type_offering,
-        #     is_active=True
-        # )
+        cls.news_email_daily_offering_without_area = cls.user_dict['user_0'].news_email
+        cls.news_email_daily_offering_without_area.province = cls.province
+        cls.news_email_daily_offering_without_area.municipality = cls.municipality
+        cls.news_email_daily_offering_without_area.interval = cls.interval_daily
+        cls.news_email_daily_offering_without_area.ad_type = cls.ad_type_offering
+        cls.news_email_daily_offering_without_area.is_active = True
+        cls.news_email_daily_offering_without_area.save()
 
-        # cls.news_email_daily_offering_with_area.areas.add(cls.area)
 
-        # ### Weekly 
-        # cls.interval_weekly = IntervalChoices.WEEKLY
+        cls.news_email_daily_offering_with_area = cls.user_dict['user_1'].news_email
+        cls.news_email_daily_offering_with_area.province = cls.province
+        cls.news_email_daily_offering_with_area.municipality = cls.municipality
+        cls.news_email_daily_offering_with_area.interval = cls.interval_daily
+        cls.news_email_daily_offering_with_area.ad_type = cls.ad_type_offering
+        cls.news_email_daily_offering_with_area.is_active = True
+        cls.news_email_daily_offering_with_area.areas.add(cls.area)
+        cls.news_email_daily_offering_with_area.save()
+    
 
-        # #### subscription without areas
-        # cls.news_email_daily_offering_without_area = create_news_email( 
-        #     user=cls.user_dict[2],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_weekly,
-        #     ad_type=cls.ad_type_offering,
-        #     is_active=True
-        # )
+        ### Weekly 
+        cls.interval_weekly = IntervalChoices.WEEKLY
 
-        # #### subscription with areas
-        # cls.news_email_daily_offering_with_area = create_news_email( 
-        #     user=cls.user_dict[3],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_weekly,
-        #     ad_type=cls.ad_type_offering,
-        #     is_active=True
-        # )
+        cls.news_email_daily_offering_without_area = cls.user_dict['user_2'].news_email
+        cls.news_email_daily_offering_without_area.province = cls.province
+        cls.news_email_daily_offering_without_area.municipality = cls.municipality
+        cls.news_email_daily_offering_without_area.interval = cls.interval_weekly
+        cls.news_email_daily_offering_without_area.ad_type = cls.ad_type_offering
+        cls.news_email_daily_offering_without_area.is_active = True
+        cls.news_email_daily_offering_without_area.save()
 
-        # cls.news_email_daily_offering_with_area.areas.add(cls.area)
+
+        cls.news_email_daily_offering_with_area = cls.user_dict['user_3'].news_email
+        cls.news_email_daily_offering_with_area.province = cls.province
+        cls.news_email_daily_offering_with_area.municipality = cls.municipality
+        cls.news_email_daily_offering_with_area.interval = cls.interval_weekly
+        cls.news_email_daily_offering_with_area.ad_type = cls.ad_type_offering
+        cls.news_email_daily_offering_with_area.is_active = True
+        cls.news_email_daily_offering_with_area.areas.add(cls.area)
+        cls.news_email_daily_offering_with_area.save()
 
 
         # ##### Requesting #####
-        # cls.ad_type_requesting = AdTypesChoices.REQUESTING
+        cls.ad_type_requesting = AdTypesChoices.REQUESTING
 
-        # #### subscription without areas
-        # cls.news_email_daily_offering_without_area = create_news_email( 
-        #     user=cls.user_dict[4],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_daily,
-        #     ad_type=cls.ad_type_requesting,
-        #     is_active=True
-        # )
-        
-        # #### subscription with areas
-        # cls.news_email_daily_offering_with_area = create_news_email( 
-        #     user=cls.user_dict[5],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_daily,
-        #     ad_type=cls.ad_type_requesting,
-        #     is_active=True
-        # )
 
-        # cls.news_email_daily_offering_with_area.areas.add(cls.area)
+        ### Daily 
+        cls.news_email_daily_offering_without_area = cls.user_dict['user_4'].news_email
+        cls.news_email_daily_offering_without_area.province = cls.province
+        cls.news_email_daily_offering_without_area.municipality = cls.municipality
+        cls.news_email_daily_offering_without_area.interval = cls.interval_daily
+        cls.news_email_daily_offering_without_area.ad_type = cls.ad_type_requesting
+        cls.news_email_daily_offering_without_area.is_active = True
+        cls.news_email_daily_offering_without_area.save()
+
+
+        cls.news_email_daily_offering_with_area = cls.user_dict['user_5'].news_email
+        cls.news_email_daily_offering_with_area.province = cls.province
+        cls.news_email_daily_offering_with_area.municipality = cls.municipality
+        cls.news_email_daily_offering_with_area.interval = cls.interval_daily
+        cls.news_email_daily_offering_with_area.ad_type = cls.ad_type_requesting
+        cls.news_email_daily_offering_with_area.is_active = True
+        cls.news_email_daily_offering_with_area.areas.add(cls.area)
+        cls.news_email_daily_offering_with_area.save()
 
         # ### Weekly 
-        # cls.interval_weekly = IntervalChoices.WEEKLY
+        cls.interval_weekly = IntervalChoices.WEEKLY
 
-        # #### subscription without areas
-        # cls.news_email_daily_offering_without_area = create_news_email( 
-        #     user=cls.user_dict[6],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_weekly,
-        #     ad_type=cls.ad_type_requesting,
-        #     is_active=True
-        # )
+        cls.news_email_daily_offering_without_area = cls.user_dict['user_6'].news_email
+        cls.news_email_daily_offering_without_area.province = cls.province
+        cls.news_email_daily_offering_without_area.municipality = cls.municipality
+        cls.news_email_daily_offering_without_area.interval = cls.interval_weekly
+        cls.news_email_daily_offering_without_area.ad_type = cls.ad_type_requesting
+        cls.news_email_daily_offering_without_area.is_active = True
+        cls.news_email_daily_offering_without_area.save()
 
-        # #### subscription with areas
-        # cls.news_email_daily_offering_with_area = create_news_email( 
-        #     user=cls.user_dict[7],
-        #     province=cls.province,
-        #     municipality=cls.municipality,
-        #     interval=cls.interval_weekly,
-        #     ad_type=cls.ad_type_requesting,
-        #     is_active=True
-        # )
 
-        # cls.news_email_daily_offering_with_area.areas.add(cls.area)
-
+        cls.news_email_daily_offering_with_area = cls.user_dict['user_7'].news_email
+        cls.news_email_daily_offering_with_area.province = cls.province
+        cls.news_email_daily_offering_with_area.municipality = cls.municipality
+        cls.news_email_daily_offering_with_area.interval = cls.interval_weekly
+        cls.news_email_daily_offering_with_area.ad_type = cls.ad_type_requesting
+        cls.news_email_daily_offering_with_area.is_active = True
+        cls.news_email_daily_offering_with_area.areas.add(cls.area)
+        cls.news_email_daily_offering_with_area.save()
 
 
 
